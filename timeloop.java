d4d073984b1cd8cc4c92a7a5e495adc825a0db8a
jackson-ong-kh <84653325+jackson-ong-kh@users.noreply.github.com>
2022-06-12 23:28:39 +0800

    Create timeloop.java

---
+++ timeloop.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class timeloop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int times = sc.nextInt();
+        for (int i = 1; i < times+1; i++) System.out.printf("%s Abracadabra\n", i);
+    }
+}
