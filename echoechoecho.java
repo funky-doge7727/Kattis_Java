249e84d9114f81622d47cf636bae230dfb78aa41
jackson-ong-kh <84653325+jackson-ong-kh@users.noreply.github.com>
2022-06-13 00:13:25 +0800

    Create echoechoecho.java

---
+++ echoechoecho.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class echoechoecho {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String word = sc.next();
+        for (int i = 0; i < 3; i++) System.out.printf("%s ", word);
+    }
+}
